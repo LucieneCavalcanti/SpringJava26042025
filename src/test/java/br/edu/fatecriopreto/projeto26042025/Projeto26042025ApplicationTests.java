ad2c0887c822dac42f9e4c272c91782a42d75a4c
Djalma Silva <djalmadsilva@gmail.com>
2025-04-26 13:50:00 -0300

    Criação do projeto

---
+++ src/test/java/br/edu/fatecriopreto/projeto26042025/Projeto26042025ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecriopreto.projeto26042025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto26042025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
